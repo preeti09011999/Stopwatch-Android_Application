0856b72964a66f2673182229a5401ee7e56e64c7
Preeti <preeti09011999@gmail.com>
2020-02-11 00:32:54 +0530

    Stopwatch

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvSplash, tvSubSplash;
+    Button btnget;
+    Animation atg, btgtwo, btgone;
+    ImageView ivSplash;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvSplash = findViewById(R.id.tvSplash);
+        tvSubSplash = findViewById(R.id.tvSubSplash);
+        btnget = findViewById(R.id.btnget);
+        ivSplash = findViewById(R.id.ivSplash);
+
+        //load animation
+        atg = AnimationUtils.loadAnimation(this, R.anim.atg);
+        btgone = AnimationUtils.loadAnimation( this, R.anim.btgone);
+        btgtwo = AnimationUtils.loadAnimation(this, R.anim.btgtwo);
+
+        //passing animation
+        ivSplash.startAnimation(atg);
+        tvSplash.startAnimation(btgone);
+        tvSubSplash.startAnimation(btgone);
+        btnget.startAnimation(btgtwo);
+
+        //passing event
+        btnget.setOnClickListener(new  View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent a = new Intent(MainActivity.this, StopWatchAct.class);
+                a.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(a);
+            }
+        });
+
+        //import font
+
+        Typeface MLight = Typeface.createFromAsset(getAssets(), "fonts/MLight.ttf");
+        Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
+        Typeface MRegular = Typeface.createFromAsset(getAssets(), "fonts/MRegular.ttf");
+
+        //customize font
+        tvSplash.setTypeface(MRegular);
+        tvSubSplash.setTypeface(MLight);
+        btnget.setTypeface(MMedium);
+
+    }
+}
---
+++ app/src/main/java/com/example/stopwatch/StopWatchAct.java
@@ -0,0 +1,61 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+
+public class StopWatchAct extends AppCompatActivity {
+
+    Button btnstart, btnstop;
+    ImageView icanchor;
+    Animation roundingalone;
+    Chronometer timerHere;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stop_watch);
+
+        btnstart = findViewById(R.id.btnstart);
+        btnstop = findViewById(R.id.btnstop);
+        icanchor = findViewById(R.id.icanchor);
+        timerHere = findViewById(R.id.timerHere);
+
+        //create optional animation
+        btnstop.setAlpha(0);
+
+        //load animation
+        roundingalone = AnimationUtils.loadAnimation(this, R.anim.roundingalone);
+
+        //import font
+        Typeface MLight = Typeface.createFromAsset(getAssets(), "fonts/MLight.ttf");
+        Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
+        Typeface MRegular = Typeface.createFromAsset(getAssets(), "fonts/MRegular.ttf");
+
+        //customize font
+        btnstart.setTypeface(MMedium);
+        btnstop.setTypeface(MMedium);
+
+        btnstart.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //passing animation
+                icanchor.startAnimation(roundingalone);
+                btnstop.animate().alpha(1).translationY(-80).setDuration(300).start();
+                btnstart.animate().alpha(1).setDuration(300).start();
+                //start time
+                timerHere.setBase(SystemClock.elapsedRealtime());
+                timerHere.start();
+            }
+        });
+
+    }
+}
